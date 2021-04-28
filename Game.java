78b0b2ae480e6a8e79434e2b7f6b7548e62c80fd
star_engine <jds4873@gmail.com>
2021-04-28 14:48:14 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -22,51 +22,20 @@ public class Game {
         board = new Board(state);
         printBoard();
     }
-
-    public void humanVsHuman () {
-        HumanPlayer player1 = new HumanPlayer();
-        HumanPlayer player2 = new HumanPlayer();
+    
+    public void match(Player p1, Player p2) {
         printBoard();
-        player1.makeMove(board);
+        p1.makeMove(board);
         while (board.getGameStatus() == Board.StateOfGame.NOT_FINISHED) {
-            player2.makeMove(board);
-            if(board.getGameStatus() != Board.StateOfGame.NOT_FINISHED) {
+            p2.makeMove(board);
+            if (board.getGameStatus() != Board.StateOfGame.NOT_FINISHED) {
                 System.out.println(board.getGameStatus());
                 return;
             }
-            player1.makeMove(board);
+            p1.makeMove(board);
         }
-
         System.out.println(board.getGameStatus());
     }
-
-    /*
-    public void aiVsHuman() {
-        printBoard();
-        makeMove();
-        while (board.getGameStatus() == Board.StateOfGame.NOT_FINISHED) {
-            makeComputerMove();
-            if(board.getGameStatus() != Board.StateOfGame.NOT_FINISHED) {
-                System.out.println(board.getGameStatus());
-                return;
-            }
-            makeMove();
-        }
-
-        System.out.println(board.getGameStatus());
-    }
-     */
-
-    /*
-    public void aiVsAi () {
-        makeComputerMove();
-        while (board.getGameStatus() == Board.StateOfGame.NOT_FINISHED) {
-            makeComputerMove();
-        }
-
-        System.out.println(board.getGameStatus());
-    }
-    */
 
     private void getGameMode(String[] commands) {
         Player player1 = null;
@@ -79,6 +48,8 @@ public class Game {
             case "easy" :
                 player1 = new AIPlayerEasy();
                 break;
+            case "medium" :
+                player1 = new AIPlayerMedium();
             default :
                 return;
         }
@@ -90,9 +61,13 @@ public class Game {
             case "easy" :
                 player2 = new AIPlayerEasy();
                 break;
+            case "medium" :
+                player2 = new AIPlayerMedium();
             default :
                 return;
         }
+        
+        match(player1, player2);
     }
 
     public void menu() {
@@ -101,7 +76,7 @@ public class Game {
     }
 
     private boolean checkPlayerOption(String command) {
-        boolean valid = command.equals("user") || command.equals("easy");
+        boolean valid = command.equals("user") || command.equals("easy") || command.equals.("medium");
         if (!valid) {
             System.out.println("Bad parameters!");
         }
@@ -147,23 +122,12 @@ public class Game {
                 continue;
             }
 
-            //inputScanner.nextLine();  // Advances the scanner to prevent input errors
+            inputScanner.nextLine();  // Advances the scanner to prevent input errors
             invalid = false;  // Sets the condition to false to break the loop
 
         } while(invalid);
         return commands;
     }
-
-    /*
-    public void makeMove() {
-        int[] coordinates = getCoordinates();
-        board.markBoard(board.getCurrentPlayer(), coordinates[0], coordinates[1]);
-        printBoard();
-        //System.out.println("Making move level " + '"' + "easy" + '"');
-        board.updateGame();
-        //printGameState();
-    }
-    */
 
     public void makeComputerMove() {
         Random rand = new Random(System.currentTimeMillis());
@@ -193,38 +157,4 @@ public class Game {
         System.out.println(board.getGameStatus());
     }
 
-    /*
-    private int[] getCoordinates() {
-        int row = 0;
-        int column = 0;
-        boolean invalid = true;
-        int[] coordinates = new int[2];
-        do {
-            System.out.println("Enter the coordinates: >");
-            if (inputScanner.hasNextInt()) {  //Checks that the input can be parsed as an int
-                row = inputScanner.nextInt() - 1; //subtract 1 to account for zero-indexing in array
-                column = inputScanner.nextInt() - 1;
-
-                if (row > 2 || row < 0 || column < 0 || column > 2) {
-                    System.out.println("Coordinates should be from 1 to 3!");
-                    continue;
-                }
-
-                if (!board.cellOpen(row, column)) {
-                    System.out.println("This cell is occupied! Choose another one!");
-                    continue;
-                }
-
-                inputScanner.nextLine();  // Advances the scanner to prevent input errors
-                invalid = false;  // Sets the condition to false to break the loop
-            } else {
-                System.out.println("You should enter numbers!");
-                inputScanner.nextLine(); // Advances the scanner to prevent input errors
-            }
-        } while (invalid); // The loop ends when the input is valid
-        coordinates[0] = row;
-        coordinates[1] = column;
-        return coordinates;
-    }
-    */
 }
