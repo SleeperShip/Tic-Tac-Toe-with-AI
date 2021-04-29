c6651c7f405e67eff3208f9451b86be7ecaa0e10
star_engine <jds4873@gmail.com>
2021-04-29 12:28:37 -0600

    Update AIPlayerMedium.java

--- AIPlayerMedium.java
+++ AIPlayerMedium.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class AIPlayerMedium extends Player {
    
     private Random rand = new Random();
@@ -40,42 +38,46 @@ public void makeRandomMove(Board board) {
     board.markBoard(board.getCurrentPlayer(), row, col);
     board.updateGame();
     board.printBoard();
-    System.out.println("Making move level " + '"' + "easy" + '"');
+    System.out.println("Making move level " + '"' + "medium(random)" + '"');
     board.updateGame();
 }
 
 public int[] checkForWinningCell(Board board) {
    char[][] grid = board.getGrid();
    char currentPlayer = board.getCurrentPlayer();
+   char opposingPlayer = board.getOpposingPlayer();
    int[] winningLocation = new int[2];
    char[] tempRow = new char[3];
    winningLocation[0] = -1;
    winningLocation[1] = -1;
    
    for (int i = 0; i < grid[0].length; i++) {
-       if (checkArrayRow(grid[i], currentPlayer)) {
+       if (checkArrayRow(grid[i], currentPlayer, opposingPlayer)) {
            winningLocation = getBlankSpaceRow(grid[i], i);
        }
    }
  
    for (int i = 0; i < grid[0].length; i++) {
-       if (checkArrayCol(grid, i, currentPlayer)) {
+       if (checkArrayCol(grid, i, currentPlayer, opposingPlayer)) {
            winningLocation = getBlankSpaceCol(grid, i);
        }
    }
    
-   if (checkArrayDiag1(grid, currentPlayer)) {
+   if (checkArrayDiag1(grid, currentPlayer, opposingPlayer)) {
        winningLocation = getBlankSpaceDiag1(grid);
    }
    
-   if (checkArrayDiag2(grid, currentPlayer)) {
+   if (checkArrayDiag2(grid, currentPlayer, opposingPlayer)) {
        winningLocation = getBlankSpaceDiag2(grid);
    }
    
    return winningLocation;
 }
 
-public int[] getBlankSpaceDiag1(char[][] grid) {
+/* the 4 blank space methods are only utilized 
+when a win condition/loss prevention spot is found */
+
+public int[] getBlankSpaceDiag1(char[][] grid) { 
    int[] coordinates = new int[2];
    
    for (int i = 0; i < grid[0].length; i++)  {
@@ -127,13 +129,18 @@ public int[] getBlankSpaceRow(char[] set, int row) {  //row number is given, ret
    return coordinates;
 }
 
-public boolean checkArrayDiag1(char[][] grid, char currentPlayer) {
-   int playerCount = 0;
+public boolean checkArrayDiag1(char[][] grid, char currentPlayer, char opposingPlayer) { //DUN
+   int currentPlayerCount = 0;
+   int opposingPlayerCount = 0;
    int whiteSpaceCount = 0;
    
    for (int i = 0; i < grid[0].length; i++) {
-      if(grid[i][i] == currentPlayer) {
-           playerCount++;
+      if (grid[i][i] == currentPlayer) {
+           currentPlayerCount++;
+      }
+      
+      if (grid[i][i] == opposingPlayer) {
+          opposingPlayerCount++;
       }
            
       if (grid[i][i] == ' ') {
@@ -141,20 +148,25 @@ public boolean checkArrayDiag1(char[][] grid, char currentPlayer) {
       }
   }
    
-   if (playerCount == 2 && whiteSpaceCount == 1) {
+   if ( (currentPlayerCount == 2 && whiteSpaceCount == 1) || (opposingPlayerCount == 2 && whiteSpaceCount == 1) ) {
        return true;
    }
    
    return false;
 }
 
-public boolean checkArrayDiag2(char[][] grid, char currentPlayer) {
-   int playerCount = 0;
+public boolean checkArrayDiag2(char[][] grid, char currentPlayer, char opposingPlayer) {
+   int currentPlayerCount = 0;
    int whiteSpaceCount = 0;
+   int opposingPlayerCount = 0;
    
    for (int i = 0; i < grid[0].length; i++) {
            if(grid[grid[0].length - i - 1][i] == currentPlayer) {
                playerCount++;
+           }
+           
+           if (grid[grid[0].length - i - 1][i] == opposingPlayer) {
+               opposingPlayerCount++;
            }
            
            if (grid[grid[0].length - i - 1][i] == ' ') {
@@ -163,45 +175,52 @@ public boolean checkArrayDiag2(char[][] grid, char currentPlayer) {
        
    }
    
-   if (playerCount == 2 && whiteSpaceCount == 1) {
+   if ( (currentPlayerCount == 2 && whiteSpaceCount == 1) || (opposingPlayerCount == 2 && whiteSpaceCount == 1) ) {
        return true;
    }
    
    return false;
 }
 
-public boolean checkArrayCol(char[][] grid, int colNumber, char currentPlayer) {
-   int playerCount = 0;
+public boolean checkArrayCol(char[][] grid, int colNumber, char currentPlayer, char opposingPlayer) {
+   int currentPlayerCount = 0;
    int whiteSpaceCount = 0;
-   boolean winningColumn = false;
+   int opposingPlayerCount = 0;
    
-   for (int i = 0; i < grid[0].length; i++) {    //use Board.SIZE
+   for (int i = 0; i < grid[0].length; i++) {   
       if (grid[i][colNumber] == currentPlayer) {
-          playerCount++;
+          currentPlayerCount++;
       }
+          
+      if (grid[i][colNumber] == opposingPlayer) {
+          opposingPlayerCount++;
+      }  
            
       if (grid[i][colNumber] == ' ') {
           whiteSpaceCount++;
       }
    }
    
-   if (playerCount == 2 && whiteSpaceCount == 1) {
-       winningColumn = true;
-       return winningColumn;
+   if ( (currentPlayerCount == 2 && whiteSpaceCount == 1) || (opposingPlayerCount == 2 && whiteSpaceCount == 1) ) {
+       return true;
    }
    
-   return winningColumn;
+   return false;
 }
 
-public boolean checkArrayRow(char[] set, char currentPlayer) {
-   int playerCount = 0;
+public boolean checkArrayRow(char[] set, char currentPlayer, char opposingPlayer) {
+   int currentPlayerCount = 0;
    int whiteSpaceCount = 0;
+   int opposingPlayerCount = 0;
    
    for (int i = 0; i < set.length; i++) {  
 
+       if (set[i] == opposingPlayer) {
+           opposingPlayerCount++;
+       }
 
        if (set[i] == currentPlayer) {
-           playerCount++;
+           currentPlayerCount++;
        }
        
        if(set[i] == ' ') {
@@ -209,7 +228,7 @@ public boolean checkArrayRow(char[] set, char currentPlayer) {
        }
    }
    
-   if (playerCount == 2 && whiteSpaceCount == 1) {
+   if ( (currentPlayerCount == 2 && whiteSpaceCount == 1) || (opposingPlayerCount == 2 && whiteSpaceCount == 1) ) {
        return true;
    }
    
